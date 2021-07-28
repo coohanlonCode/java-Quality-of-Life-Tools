32c6095c1f1b190e597f4172e5a2fd269ef1c604
Colin O'Hanlon <coohanlonCode@google.com>
2021-07-28 09:32:12 -0400

    Initial commit

--- src/main/java/com/java/qolTools/files/PassFailedSkippedTextLineParser.java
+++ src/main/java/com/java/qolTools/files/PassFailedSkippedTextLineParser.java
@@ -3,6 +3,6 @@ package com.java.qolTools.files;
 public class PassFailedSkippedTextLineParser {
 
     public static void main(String[] args){
-        System.out.println("Initial commit test");
+        System.out.println("Initial commit");
     }
 }
